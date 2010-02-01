05e43928edaf072697ab0157c4b69bfbb723f182
Tony Hillerson <tony.hillerson@effectiveui.com>
2010-02-01 16:49:13 -0700

    trying passenger web app instead of web app

--- peeps/recipes/default.rb
+++ peeps/recipes/default.rb
@@ -1,7 +1,7 @@
 include_recipe "passenger_enterprise"
 include_recipe "rails_enterprise"
 
-web_app "peeps" do
+passenger_web_app "peeps" do
   docroot "/home/tony/peeps/public"
   server_name "peeps.#{node[:domain]}"
   server_aliases [ "peeps", node[:hostname] ]
