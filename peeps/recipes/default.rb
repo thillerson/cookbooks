11c5760feab23454d0580c39345334b35fb52f3b
Tony Hillerson <tony.hillerson@effectiveui.com>
2010-02-01 15:37:12 -0700

    corrected a misunderstanding in chef templates and definitions

--- peeps/recipes/default.rb
+++ peeps/recipes/default.rb
@@ -1,9 +1,9 @@
 include_recipe "passenger_enterprise"
 include_recipe "rails_enterprise"
 
-web_app do
+web_app "peeps" do
   docroot "/home/tony/peeps/public"
-  server_name "peeps.local"
-  server_aliases []
+  server_name "peeps.#{node[:domain]}"
+  server_aliases [ "peeps", node[:hostname] ]
   rails_env "production"
 end
