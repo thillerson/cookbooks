eb80e0760b0592419de3689274d238a7740ccd89
Tony Hillerson <tony.hillerson@effectiveui.com>
2010-02-03 15:47:26 -0700

    better generalized recipe

--- peeps/recipes/default.rb
+++ peeps/recipes/default.rb
@@ -2,10 +2,22 @@ include_recipe "passenger_enterprise::apache2"
 include_recipe "rails_enterprise"
 include_recipe "mysql::server"
 
-web_app "peeps" do
-  docroot "/home/tony/peeps/public"
-  server_name "peeps.#{node[:domain]}"
-  server_aliases [ "peeps", node[:hostname] ]
+# deploy directory
+directory node[:railsapp][:dir] do
+ owner node[:deploy_user][:name]
+end
+
+# capify
+%w{ releases shared shared/system shared/pids shared/log }.each do |dir|
+  directory "#{node[:railsapp][:dir]}/#{dir}" do
+    owner node[:deploy_user][:name]
+  end
+end
+
+web_app node[:railsapp][:name] do
+  docroot "#{node[:railsapp][:dir]}/public"
+  server_name "#{node[:railsapp][:name]}.#{node[:domain]}"
+  server_aliases [ node[:railsapp][:name], node[:hostname] ]
   rails_env "production"
 end
 
